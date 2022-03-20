f1391ee04d35ded84f8cfa3e7731f4392f5f9581
chaehoon <youspend@gmail.com>
2022-03-20 11:08:47 +0900

    feat : axios client에서 token 삽입 interceptor 작성

--- src/libs/FrontApiClient.ts
+++ src/libs/FrontApiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const frontApiClient = (() => {
-  return axios.create({
+  const client = axios.create({
     // baseURL: 'http://api.foorun.co.kr',
     baseURL: 'http://localhost:8088',
     withCredentials: true,
@@ -9,6 +9,17 @@ const frontApiClient = (() => {
       'Content-Type': 'application/json'
     }
   });
+  client.interceptors.request.use(
+    config => {
+      config.headers = Object.assign(config.headers, {
+        'Authorization': localStorage.getItem('token'),
+        'X-Refresh-Token': localStorage.getItem('refreshToken'),
+      })
+      return config
+    },
+    error => Promise.reject(error)
+  )
+  return client
 })()
 
 export default frontApiClient
